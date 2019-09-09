e82827af6a6475dcc4b35cb204e0fbfc4baf2110
BrunoTonche <brunocasas6@gmail.com>
2019-09-09 20:08:05 -0300

    Inserindo o projeto no GIT

---
+++ CamadaDados/DCategoria.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CamadaDados
+{
+    class DCategoria
+    {
+        private int _Idcategoria;
+        private string _Nome;
+        private string _Descricao;
+        private string _TextoBuscar;
+
+        public int Idcategoria
+        { get => _Idcategoria; set => _Idcategoria = value; }
+
+        public string Nome
+        { get => _Nome; set => _Nome = value; }
+
+        public string Descricao
+        { get => _Descricao; set => _Descricao = value; }
+
+        public string TextoBuscar
+        { get => _TextoBuscar; set => _TextoBuscar = value; }
+
+
+        //Construtor Vazio
+        public DCategoria()
+        {
+
+        }
+
+        //Construtor com Parametros
+        
+        public DCategoria(int idcategoria, string nome, string descricao, string textobuscar)
+        {
+            this.Idcategoria = idcategoria;
+            this.Nome =  nome;
+            this.Descricao = descricao;
+            this.TextoBuscar = textobuscar;
+        }
+
+        //Metodo Inserir
+
+        public string Inserir(DCategoria Categoria)
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //codigo
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spinserir_categoria";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNome = new SqlParameter();
+                ParNome.ParameterName = "@nome";
+                ParNome.SqlDbType = SqlDbType.VarChar;
+                ParNome.Size = 100;
+                ParNome.Value = Categoria.Nome;
+                SqlCmd.Parameters.Add(ParNome);
+
+                SqlParameter ParDescricao = new SqlParameter();
+                ParDescricao.ParameterName = "@descricao";
+                ParDescricao.SqlDbType = SqlDbType.VarChar;
+                ParDescricao.Size = 50;
+                ParDescricao.Value = Categoria.Descricao;
+                SqlCmd.Parameters.Add(ParDescricao);
+
+                //Executar o comando
+
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "Ok" : "Resgistro não foi Inserido";
+            }
+            catch(Exception ex)
+            {
+                resp = ex.Message;
+            }
+
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        //Metodo Editar 
+
+        public string Editar(DCategoria Categoria)
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //codigo
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "speditar_categoria";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNome = new SqlParameter();
+                ParNome.ParameterName = "@nome";
+                ParNome.SqlDbType = SqlDbType.VarChar;
+                ParNome.Size = 100;
+                ParNome.Value = Categoria.Nome;
+                SqlCmd.Parameters.Add(ParNome);
+
+                SqlParameter ParDescricao = new SqlParameter();
+                ParDescricao.ParameterName = "@descricao";
+                ParDescricao.SqlDbType = SqlDbType.VarChar;
+                ParDescricao.Size = 50;
+                ParDescricao.Value = Categoria.Descricao;
+                SqlCmd.Parameters.Add(ParDescricao);
+
+                //Executar o comando
+
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "Ok" : "Resgistro não foi Inserido";
+            }
+            catch (Exception ex)
+            {
+                resp = ex.Message;
+            }
+
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        //Metodo Excluir 
+
+        public string Excluir(DCategoria Categoria)
+        {
+
+        }
+
+        //Metodo Mostrar 
+
+        public DataTable Mostrar(DCategoria Categoria)
+        {
+
+        }
+
+        //Metodo Buscar Nome 
+
+        public string BuscarNome(DCategoria Categoria)
+        {
+
+        }
+    }
+}
